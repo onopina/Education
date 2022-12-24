beeb1796d7acf2813e2b320a40b71726deb23e6c
onopina <sochiboston@gmail.com>
2022-12-24 13:44:25 +0300

    Homework

---
+++ src/inheritance_9_1_1/Main.java
@@ -0,0 +1,15 @@
+package inheritance_9_1_1;
+
+public class Main
+{
+	public static void main(String[] args) {
+		
+		Employee employee1 = new Employee();
+		employee1.printClassName();
+		Doctor doctor1 = new Doctor();
+		doctor1.printClassName();
+		Programmer programmer1 = new Programmer();
+		programmer1.printClassName();
+		
+	}
+}
---
+++ src/inheritance_9_1_1/Programmer.java
@@ -0,0 +1,24 @@
+package inheritance_9_1_1;
+
+public class Programmer extends Employee {
+    
+    private int experience;
+    
+    public int getExperience() {
+        return experience;
+    }
+    
+    public void setExperience(int experience) {
+        if (experience >= 0){
+            this.experience = experience;
+        }
+        else {
+            this.experience = 0;
+        }
+    }
+    
+    public void printClassName() {
+         System.out.println("Class Programmer");
+     }
+    
+}
