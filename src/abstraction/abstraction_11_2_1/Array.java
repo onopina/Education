795c68dea97abe98b88aee0b15ce1b5094e0949d
onopina <sochiboston@gmail.com>
2023-01-15 20:30:36 +0300

    Homework

--- src/abstraction/abstraction_11_2_1/Array.java
+++ src/abstraction/abstraction_11_2_1/Array.java
@@ -13,10 +13,12 @@ public class Array implements Service {
         Scanner scanner = new Scanner(System.in);
         size = scanner.nextInt();
      //   int []array = new int[size];
+        int []array1 = new int[size];
         System.out.println("Enter the elements of the array: ");
         for(int i=0; i<size; i++){
-            array[i] = scanner.nextInt();
+            array1[i] = scanner.nextInt();
         }
+        array = array1;
       //  System.out.println(Arrays.toString(array));
     }
 
