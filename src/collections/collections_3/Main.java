cbb648f5e02eabe6c6220a3610452145c72cd209
onopina <sochiboston@gmail.com>
2023-06-20 14:43:39 +0300

    Homework

---
+++ src/collections/collections_3/Main.java
@@ -0,0 +1,22 @@
+package collections.collections_3;
+
+import java.util.*;
+
+/**
+ * 3.	Дано дерево, содержащее числа. Вывести его в обратном порядке.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Set<Integer> set = new TreeSet<>();
+        set.add(15);
+        set.add(5);
+        set.add(10);
+        set.add(30);
+        set.add(25);
+        set.add(20);
+
+        List<Integer> list = new ArrayList<>(set);
+        Collections.reverse(list);
+        System.out.println(list);
+    }
+}
---
+++ src/collections/collections_4/Main.java
@@ -0,0 +1,21 @@
+package collections.collections_4;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * 4.	*Дан массив с повторениями. Отсортировать его за O(n) (учитывая повторения).
+ */
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {4, 2, 8, -2, 4, 6, 8, 0, -2, 10};
+
+        Set<Integer> set = new TreeSet<>();
+
+        for (int num : arr) {
+            set.add(num);
+        }
+
+        System.out.println(set);
+    }
+}
